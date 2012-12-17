4d10fbbdbf6fb2fc82ef16f3a85c70ff6bf7a550
Rafał Magda <rafalmag@gmail.com>
2012-12-17 14:41:53 +0100

    vadin hello world
    --HG--
    branch : vaadin

---
+++ xmas-vaadin/src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+import com.vaadin.Application;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class HelloWorld extends Application {
+
+    /**
+     * Init is invoked on application load (when a user accesses the application
+     * for the first time).
+     */
+    @Override
+    public void init() {
+
+        // Main window is the primary browser window
+        final Window main = new Window("Hello window");
+        setMainWindow(main);
+
+        // "Hello world" text is added to window as a Label component
+        main.addComponent(new Label("Hello World!"));
+    }
+
+}
