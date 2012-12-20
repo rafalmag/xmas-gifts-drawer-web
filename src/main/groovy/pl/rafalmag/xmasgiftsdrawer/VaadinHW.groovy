76d3537c93fd5c0efdd64949b2dd3d5a81feb0d5
Rafał Magda <rafalmag@gmail.com>
2012-12-20 19:58:07 +0100

    table data extracted to IndexedContainer
    --HG--
    branch : vaadin

--- src/main/groovy/pl/rafalmag/xmasgiftsdrawer/VaadinHW.groovy
+++ src/main/groovy/pl/rafalmag/xmasgiftsdrawer/VaadinHW.groovy
@@ -2,6 +2,8 @@ package pl.rafalmag.xmasgiftsdrawer
 
 import com.google.common.collect.ImmutableList
 import com.vaadin.Application
+import com.vaadin.data.Item
+import com.vaadin.data.util.IndexedContainer
 import com.vaadin.ui.Label
 import com.vaadin.ui.Table
 import com.vaadin.ui.Window
@@ -28,26 +30,14 @@ class VaadinHW extends Application {
         main.addComponent(table)
     }
 
-    Table initTable() {
+    static Table initTable() {
         Table table = new Table("Table with Cell Styles");
         table.addStyleName("checkerboard");
 
-// Add some columns in the table. In this example, the property
-// IDs of the container are integers so we can determine the
-// column number easily.
 
-//        table.addContainerProperty("0", String.class, null, "", null, null);
-        for (int i = 0; i < 8; i++)
-            table.addContainerProperty("" + (i + 1), String.class, null,
-                    String.valueOf((char) (65 + i)), null, null);
+        IndexedContainer container = initContainer()
 
-// Add some items in the table.
-        table.addItem(["R", "N", "B", "Q", "K", "B", "N", "R"].toArray(), new Integer(0));
-        table.addItem(["P", "P", "P", "P", "P", "P", "P", "P"].toArray(), new Integer(1));
-        for (int i = 2; i < 6; i++)
-            table.addItem(["", "", "", "", "", "", "", ""].toArray(), new Integer(i));
-        table.addItem(["P", "P", "P", "P", "P", "P", "P", "P"].toArray(), new Integer(6));
-        table.addItem(["R", "N", "B", "Q", "K", "B", "N", "R"].toArray(), new Integer(7));
+        table.setContainerDataSource(container)
 
         table.setRowHeaderMode(Table.ROW_HEADER_MODE_ID)
         table.setColumnHeaderMode(Table.COLUMN_HEADER_MODE_ID)
@@ -75,5 +65,38 @@ class VaadinHW extends Application {
             }
         })
         table
+    }
+
+    static IndexedContainer initContainer() {
+        IndexedContainer container = new IndexedContainer();
+// Add some columns in the table. In this example, the property
+// IDs of the container are integers so we can determine the
+// column number easily.
+
+        def properties = (1..8).step(1).collect { "" + it }
+        for (int i = 0; i < 8; i++) {
+            String property = properties[i]
+            String value = String.valueOf((char) (65 + i))
+            container.addContainerProperty(property, property.getClass(), value)
+        }
+
+// Add some items in the table.
+        addItems(container, properties, new Integer(0), ["R", "N", "B", "Q", "K", "B", "N", "R"])
+        addItems(container, properties, new Integer(1), ["P", "P", "P", "P", "P", "P", "P", "P"])
+        for (int i = 2; i < 6; i++) {
+            addItems(container, properties, new Integer(i), ["", "", "", "", "", "", "", ""])
+        }
+        addItems(container, properties, new Integer(6), ["P", "P", "P", "P", "P", "P", "P", "P"])
+        addItems(container, properties, new Integer(7), ["R", "N", "B", "Q", "K", "B", "N", "R"])
+        container
+    }
+
+    static void addItems(IndexedContainer container, List<String> properties, int itemId, ArrayList<String> values) {
+        Item item = container.addItem(itemId);
+        [properties, values].transpose().collectEntries {
+            it
+        }.each {
+            item.getItemProperty(it.key).setValue(it.value)
+        }
     }
 }
