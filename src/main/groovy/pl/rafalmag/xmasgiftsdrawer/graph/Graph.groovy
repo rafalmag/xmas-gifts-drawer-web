ccf328ef70bb2abcc45b6703cf4f239f66918e85
Rafal Magda <rafalmag@gmail.com>
2015-07-29 12:51:25 +0200

    test fixed

--- src/main/groovy/pl/rafalmag/xmasgiftsdrawer/graph/Graph.groovy
+++ src/main/groovy/pl/rafalmag/xmasgiftsdrawer/graph/Graph.groovy
@@ -1,5 +1,7 @@
 package pl.rafalmag.xmasgiftsdrawer.graph
 
+import com.google.common.collect.BiMap
+import com.google.common.collect.HashBiMap
 import grph.Grph
 import grph.in_memory.InMemoryGrph
 import pl.rafalmag.xmasgiftsdrawer.Model
@@ -7,15 +9,15 @@ import pl.rafalmag.xmasgiftsdrawer.Person
 
 public class Graph {
 
-    Map<Person, Integer> nodes
+    BiMap<Person, Integer> nodes
     Grph graph;
 
     Graph(Model model) {
         graph = new InMemoryGrph(model.toString(), true, Grph.DIRECTION.out)
-        nodes = model.persons.collectEntries {
+        nodes = HashBiMap.<Person,Integer>create(model.persons.collectEntries {
             def vertex = graph.addVertex()
             [(it): vertex]
-        }
+        })
         for (Person giver : model.persons) {
             for (Person receiver : model.persons) {
                 if (model.canGive(giver, receiver)) {
--- src/test/groovy/pl/rafalmag/xmasgiftsdrawer/graph/GraphTest.groovy
+++ src/test/groovy/pl/rafalmag/xmasgiftsdrawer/graph/GraphTest.groovy
@@ -2,7 +2,6 @@ package pl.rafalmag.xmasgiftsdrawer.graph
 
 import pl.rafalmag.xmasgiftsdrawer.ModelLoader
 import pl.rafalmag.xmasgiftsdrawer.ModelLoaderTest
-import pl.rafalmag.xmasgiftsdrawer.Person
 import spock.lang.Specification
 
 class GraphTest extends Specification {
@@ -13,10 +12,6 @@ class GraphTest extends Specification {
         def modelLoader = new ModelLoader(streamToModel)
         def model = modelLoader.load()
         streamToModel.close()
-        def a = new Person("A")
-        def b = new Person("B")
-        def c = new Person("C")
-        def d = new Person("D")
         when:
         def graph = new Graph(model);
         then:
@@ -24,25 +19,13 @@ class GraphTest extends Specification {
         graph.graph.vertices.size() == 4
         graph.graph.isDirectedSimpleGraph()
 // check directions
-        !graph.graph.isDirectedSimpleEdge(graph.nodes[a], graph.nodes[a]) // seems obvious
-        graph.graph.isDirectedSimpleEdge(graph.nodes[a], graph.nodes[b])
-        graph.graph.isDirectedSimpleEdge(graph.nodes[a], graph.nodes[c])
-        graph.graph.isDirectedSimpleEdge(graph.nodes[a], graph.nodes[d])
-
-        graph.graph.isDirectedSimpleEdge(graph.nodes[b], graph.nodes[a])
-        !graph.graph.isDirectedSimpleEdge(graph.nodes[b], graph.nodes[b]) // seems obvious
-        !graph.graph.isDirectedSimpleEdge(graph.nodes[b], graph.nodes[c]) // special rule in model.csv
-        graph.graph.isDirectedSimpleEdge(graph.nodes[b], graph.nodes[d])
-
-        graph.graph.isDirectedSimpleEdge(graph.nodes[c], graph.nodes[a])
-        graph.graph.isDirectedSimpleEdge(graph.nodes[c], graph.nodes[b])
-        !graph.graph.isDirectedSimpleEdge(graph.nodes[c], graph.nodes[c]) // seems obvious
-        graph.graph.isDirectedSimpleEdge(graph.nodes[c], graph.nodes[d])
-
-        graph.graph.isDirectedSimpleEdge(graph.nodes[d], graph.nodes[a])
-        graph.graph.isDirectedSimpleEdge(graph.nodes[d], graph.nodes[b])
-        graph.graph.isDirectedSimpleEdge(graph.nodes[d], graph.nodes[c])
-        !graph.graph.isDirectedSimpleEdge(graph.nodes[d], graph.nodes[d])  // seems obvious
+        graph.graph.getEdges().every {
+            def vertices = graph.graph.getVerticesIncidentToEdge(it.value).collect { it.value }
+            def giver = graph.nodes.inverse()[vertices[0]]
+            def getter = graph.nodes.inverse()[vertices[1]]
+            assert model.canGive(giver, getter)
+            true
+        }
 
     }
 }
