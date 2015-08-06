943f629c7b6e2b060cf6f01c80dc781b4b4fdc66
Rafal Magda <rafalmag@gmail.com>
2015-08-07 00:59:14 +0200

    ModelToModelIslands2Test + minor optimization

--- src/main/groovy/pl/rafalmag/xmasgiftsdrawer/graph/ModelToModelIslands2.groovy
+++ src/main/groovy/pl/rafalmag/xmasgiftsdrawer/graph/ModelToModelIslands2.groovy
@@ -6,15 +6,18 @@ import pl.rafalmag.xmasgiftsdrawer.Model
 class ModelToModelIslands2 implements ModelToModelIslands {
 
     List<Model> modelToModelIslands(Model model) {
-        List<Model> result = []
         def graph = new Graph2(model)
         def connectedComponents = new ConnectedComponents(graph.graph)
         def count = connectedComponents.getConnectedComponentsCount()
+        if (count == 1) {
+            return [model]
+        }
         def giantIslandNodes = connectedComponents.getGiantComponent()
         def giantIslandPersons = giantIslandNodes.collect { Graph2.getPerson(it) }
+        List<Model> result = []
         result.add(new Model(giantIslandPersons, model))
         if (count > 1) {
-            def otherPersons = new ArrayList<>(model.persons)
+            def otherPersons = model.persons.toList()
             otherPersons.removeAll(giantIslandPersons)
             result.addAll(modelToModelIslands(new Model(otherPersons, model)))
         }
