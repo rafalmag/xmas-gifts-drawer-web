c822c1d8574939b0ea69a4ad563b7d5a64943dc6
Rafal Magda <rafalmag@gmail.com>
2015-08-06 21:27:50 +0200

    acceptance split

---
+++ src/test/groovy/pl/rafalmag/xmasgiftsdrawer/algorithms/DrawerSolutionWithIslandsTest.groovy
@@ -0,0 +1,44 @@
+package pl.rafalmag.xmasgiftsdrawer.algorithms
+
+import pl.rafalmag.xmasgiftsdrawer.GiverReceiver
+import pl.rafalmag.xmasgiftsdrawer.Model
+import pl.rafalmag.xmasgiftsdrawer.Person
+import spock.lang.Shared
+import spock.lang.Specification
+
+class DrawerSolutionWithIslandsTest extends Specification {
+
+    @Shared
+    def a = new Person("A")
+    @Shared
+    def b = new Person("B")
+    @Shared
+    def c = new Person("C")
+    @Shared
+    def d = new Person("D")
+    @Shared
+    def model
+
+    def setupSpec() {
+        model = new Model()
+        model.setCanGive(c, a)
+        model.setCanGive(a, c)
+        model.setCanGive(b, d)
+        model.setCanGive(d, b)
+    }
+
+    def "should generate GiverReceiver find the only one valid combination"() {
+        when:
+        def giversReceivers = drawer.draw()
+        then:
+        giversReceivers.isValid(model)
+
+        giversReceivers.pairs.sort() == [new GiverReceiver(b, d),
+                                         new GiverReceiver(d, b),
+                                         new GiverReceiver(a, c),
+                                         new GiverReceiver(c, a)].sort()
+        where:
+        drawer << [new RandomDrawer(model), new Hamilton2Drawer(model)]
+    }
+
+}
